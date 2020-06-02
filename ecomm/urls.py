22f045b1382529d470ff11c53e876708f6135982
atulya22 <atulya.shetty@hotmail.com>
2020-06-02 19:02:40 -0400

    Setup structure for ecomm website using bootstrap md

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -1,8 +1,11 @@
-from .views import item_list
+from .views import home_page, checkout_view, product_view
 
 from django.urls import path
 app_name = 'ecomm'
 
 urlpatterns = [
-    path('', item_list, name='item_list')
+    path('', home_page, name='home-page'),
+    path('checkout/', checkout_view, name='checkout-page'),
+    path('products/', product_view, name='product-page')
+
 ]
--- ecomm/views.py
+++ ecomm/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from .models import  Item
 
 
-def item_list(request):
+def home_page(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, "ecomm/item_list.html", context)
+    return render(request, "ecomm/home-page.html", context)
+
+
+def checkout_view(request):
+    return render(request, "ecomm/checkout-page.html")
+
+def product_view(request):
+    return render(request, "ecomm/product-page.html")
