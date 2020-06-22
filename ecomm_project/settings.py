ee8b69da500780dae2c37b86fa4610de2023a569
atulya22 <atulya.shetty@hotmail.com>
2020-06-21 20:44:36 -0400

    pull secret key from environ variable

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kay9sp_^w1p#b#@ky%s*3h%iq$7x&1+-*)_8y$dqx3v6m_t@)s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
