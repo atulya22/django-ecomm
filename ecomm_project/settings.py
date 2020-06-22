09df2d9a87c0db4c01b11c3e347561cc3928c55c
atulya22 <atulya.shetty@hotmail.com>
2020-06-21 21:10:16 -0400

    Add django-heroku settings

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ LOGIN_REDIRECT_URL = '/'
 
 STRIPE_SECRET_KEY = 'sk_test_mnOsTyQ9docAoGBoYEvFoDcm00mIvxfSmd'
 STRIPE_PUBLIC_KEY = 'pk_test_CtGgbrtjGwLPocM587Gj63ny00mGIuJ4TS'
+
+django_heroku.settings(locals())
